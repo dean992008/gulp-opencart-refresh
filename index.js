1c541a8969ada229cdcd6b50d4fad75912e25afd
mac <mac@MBP15.local>
2018-10-01 23:22:11 +0300

    Fix send cookies and undefined location when incorrect login or password

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const gutil = require('gulp-util');
 const getQueryParam = require('get-query-param');
 
 let options;
+let cookie_jar = request.jar();
 
 function getKey() {
     return new Promise((resolve, reject) => {
-        var key;
         request({
             url: options.url + '/admin/index.php?route=common/login',
+            jar: cookie_jar,
             method: 'POST',
             form: {
                 username: options.login,
@@ -17,7 +18,13 @@ function getKey() {
             }
         })
             .on('response', (response) => {
-                resolve(getQueryParam('token', response.headers.location));
+                if (typeof response.headers.location === 'undefined')
+                    reject("Can't get token. Login or password incorrect");
+                else
+                    resolve(getQueryParam('token', response.headers.location));
+            })
+            .on('error', (err)=>{
+                reject(err);
             });
     });
 }
@@ -26,13 +33,34 @@ function refreshCache(key) {
     return new Promise((resolve, reject) => {
         request({
             url: options.url + '/admin/index.php?route=extension/modification/refresh&token=' + key,
+            jar: cookie_jar,
             method: 'GET'
         })
             .on('response', (response) => {
                 resolve(true);
+            })
+            .on('error', (err)=>{
+                reject(err);
             });
     });
 }
+
+function logout(key){
+    return new Promise((resolve, reject) => {
+        request({
+            url: options.url + '/admin/index.php?route=common/logout&token=' + key,
+            jar: cookie_jar,
+            method: 'GET'
+        })
+            .on('response', (response)=>{
+                resolve(true);
+            })
+            .on('error', (err)=>{
+                reject(err);
+            });
+    });
+}
+
 
 function opencartRefresh(setting) {
     if (!setting.url) {
@@ -47,13 +75,14 @@ function opencartRefresh(setting) {
     options = setting;
     getKey()
         .then(refreshCache)
+        .then(logout)
         .then((result) => {
             const time = new Date();
             const now = `[${time.getHours}:${time.getMinutes}:${time.setSeconds}] `;
             (result) ? console.log(now + 'Cache was cleared') : console.log(now + 'Something wrong');
         })
         .catch((error) => {
-            console.log(error);
+            throw new gutil.PluginError('gulp-opencart-refresh', error);
         });
     return through(function (file, encoding, callback) {
         callback(null, file);
